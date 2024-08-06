af385d0c299d24f163720a5aae76b474b4c8185f
Mansour.vn <pirouz.vn@gmail.com>
2024-08-06 22:04:29 +0330

    fit: add tags page

--- src/components/ui-kit/CheckBox.tsx
+++ src/components/ui-kit/CheckBox.tsx
@@ -1,29 +1,36 @@
 interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
   isChecked: boolean;
   label?: string;
+  id?: string | number;
 }
 
-const CheckboxOne = ({ isChecked, label, ...rest }: IProps) => {
+const CheckboxOne = ({
+  isChecked,
+  label,
+  id = "CheckboxOne",
+  ...rest
+}: IProps) => {
   return (
     <div>
       <label
-        htmlFor="checkboxLabelOne"
+        htmlFor={id}
         className="flex items-center text-xs text-gray-900 cursor-pointer select-none sm:text-sm dark:text-slate-300"
       >
         <div className="relative">
           <input
             type="checkbox"
-            id="checkboxLabelOne"
+            id={id}
             className="sr-only"
+            checked={isChecked}
             {...rest}
           />
           <div
-            className={`flex ml-2 mr-1 h-5 w-5 items-center justify-center rounded-md ring-2 ring-gray-300 dark:ring-gray-600 `}
+            className={`flex ml-2 mr-1 h-5 w-5 items-center justify-center rounded-md ring-2 ring-gray-300 dark:ring-gray-600`}
           >
             <span
               className={`h-4 w-4 rounded-md ${
                 isChecked
-                  ? "bg-gray-500 dark:bg-indigo-700 "
+                  ? "bg-gray-500 dark:bg-indigo-700"
                   : "bg-white dark:bg-gray-300"
               }`}
             ></span>
